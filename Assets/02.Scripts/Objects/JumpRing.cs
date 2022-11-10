00a8d575d0a7f55b109fc14ee5d32601e0d5ff97
paul050210 <leepaul050210@gmail.com>
2022-11-10 14:01:49 +0900

    점프 Object 기초

---
+++ Assets/02.Scripts/Objects/JumpRing.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpRing : MonoBehaviour
+{
+    private bool useFlag = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !useFlag)
+        {
+            useFlag = true;
+            //other.GetComponent<>();
+            //점프가능 트리거 초기화 함수 호출 or 트리거 변경
+
+        }
+    }
+}
