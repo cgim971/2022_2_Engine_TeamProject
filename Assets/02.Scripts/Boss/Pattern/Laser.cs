79475debdbcc055341db3f8cf545ce0e9de29cca
paul050210 <leepaul050210@gmail.com>
2022-11-15 21:35:58 +0900

    보스작업+@

---
+++ Assets/02.Scripts/Boss/Pattern/Laser.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lr;
+
+    [SerializeField] private Transform player;
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        lr.SetPosition(0, transform.position);
+        RaycastHit hit;
+        Vector3 dir = Vector3.Normalize(player.position - transform.position);
+
+        if (Physics.Raycast(transform.position, dir, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+            }
+        }
+        else
+            lr.SetPosition(1, transform.forward * 5000);
+    }
+}
+
---
+++ Assets/02.Scripts/Boss/Pattern/Pattern1.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Pattern1 : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private LineRenderer lr;
+    private MeshRenderer mesh;
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        MoveToPlayer();
+        ShootLaser();
+    }
+
+    private void MoveToPlayer()
+    {
+        transform.DOMoveX(player.transform.position.x, 1f);
+        Vector3 targetPos = player.transform.position;
+    }
+
+    private void ShowWarning()
+    {
+        mesh.material.color = new Color(0f, 0f, 0f);
+
+        mesh.material.DOColor(Color.black, 2f).SetEase(Ease.Linear);
+
+
+    }
+
+    private void ShootLaser()
+    {
+        lr.SetPosition(0, transform.position);
+        RaycastHit hit;
+        Vector3 dir = Vector3.Normalize(player.transform.position - transform.position);
+
+        if (Physics.Raycast(transform.position, dir, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+            }
+        }
+        else
+            lr.SetPosition(1, transform.forward * 5000);
+
+        if (Physics.Raycast(transform.position, dir, out hit))
+        {
+            if(hit.collider.CompareTag("Player"))
+            {
+                Debug.Log("Hit");
+            }
+        }
+    }
+
+    private IEnumerator CheckHit()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if(hit.collider.CompareTag("Player"))
+            {
+                Debug.Log("Player hit");
+            }
+
+        }   
+
+
+        
+        yield return null;
+    }    
+}
+ 
