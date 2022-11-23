8927f2058ad507625059331b3e09d631181e20cd
ChanheeKim <cgim971@gmail.com>
2022-11-23 23:32:30 +0900

    방향 전환 오브젝트 추가 및 카메라 디테일 값 추가

--- Assets/02.Scripts/Objects/Dir/ReverseDirObject.cs
+++ Assets/02.Scripts/Objects/Dir/ReverseDirObject.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurnObject : Object_Base
+public class ReverseDirObject : Object_Base
 {
     public override void EffectStart(GameObject obj)
     {
-        obj.GetComponentInParent<PlayerMovement_Base>().TurnObject();
+        obj.GetComponentInParent<PlayerMovement_Base>().ReverseDir();
     }
-
     public override void EffectEnd(GameObject obj) { }
 }
